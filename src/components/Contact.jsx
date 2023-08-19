31fc669b2369f444814d14dc363fd87b4ad4bd5a
Ying Quan <quanying26@hotmail.com>
2023-08-19 16:21:08 -0400

    contact form validation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,18 +24,21 @@ const Contact = () => {
           type="text"
           placeholder="Name"
           name="name"
+          required
         />
         <input
           className="my-4 p-2 bg-[#ccd6f6]"
           type="email"
           placeholder="Email"
           name="email"
+          required
         />
         <textarea
           className="bg-[#ccd6f6] p-10"
           name="message"
           rows="10"
           placeholder="Message"
+          required
         ></textarea>
         <button className="text-white border-1 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center">
           Let's Connect!
