89bff6cca8016cac10198cda4aee6859048097da
Ying Quan <quanying26@hotmail.com>
2023-08-19 16:11:39 -0400

    change mobile menu bg color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -54,7 +54,7 @@ const Navbar = () => {
         className={
           !nav
             ? "hidden"
-            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+            : "absolute top-0 left-0 w-full h-screen bg-purple-900 flex flex-col justify-center items-center"
         }
       >
         <li className="py-6 text-4xl">
